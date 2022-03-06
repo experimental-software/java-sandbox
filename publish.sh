a44e14af8f8722a8a4d3e23b11a48ad6c755d648
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-03-06 18:04:00 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${SCRIPT_DIR}
+
+TITLE=$(head -n 1 README.md | sed 's/# \(.*\)/\1/')
+SRC=$(find . -type f -name "*.java")
+
+gh gist create $SRC ./README.md ./build.gradle -d "${TITLE}" --web
+
+git add .
+git stash
